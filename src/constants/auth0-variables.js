9b5ccba232f0c5c3ed92c8c70cc079b8afc5dcc4
abraidotti <alessandro.braidotti@gmail.com>
2020-01-10 16:17:03 -0500

    add working app with RBAC

---
+++ src/constants/auth0-variables.js
@@ -0,0 +1,6 @@
+export const AUTH_CONFIG = {
+  domain: "kintyre-test-01.auth0.com",
+  roleUrl: "https://rbac-tutorial-app/role",
+  clientId: "yKFoE7EhdNQcdW4LyRvGYwpytu8PzJga",
+  callbackUrl: "http://localhost:3000/callback"
+};
